a8a7c2480b0fb37d6c12b90722696c291df6478e
ArleysRocaVargas <arleysroca11.1@gmail.com>
2020-09-23 18:25:16 -0500

    La página prinipal se ha dividido en componentes: header, navbar, dashboard, y footer

--- VistaPrincipal/src/app/app.module.ts
+++ VistaPrincipal/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
